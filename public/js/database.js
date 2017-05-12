7df59a318c0a2ab915c0a927655f5b145afc6461
Jaya Kirtani <jaya.kirtani@sjsu.edu>
2017-05-11 18:25:08 -0700

    Cleaned Code and added Firebase connection 1. Deleted old index.html and index.js 2. Renamed index2.html to index.html and index2.js to index.js 3. Added Firebase Project Connection details

---
+++ public/js/database.js
@@ -0,0 +1,25 @@
+var database;
+
+function fetchImages() {
+    return database.ref('/images').once('value').then(function (imageList) {
+        console.log(imageList.val());
+        return Promise.resolve(imageList.val());
+    });
+}
+
+function connectDB() {
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyBiyoO98d-j8nRV3Bmh1ZAdI-hsLjpif8o",
+        authDomain: "photosharing-c37de.firebaseapp.com",
+        databaseURL: "https://photosharing-c37de.firebaseio.com",
+        projectId: "photosharing-c37de",
+        storageBucket: "photosharing-c37de.appspot.com",
+        messagingSenderId: "1065700949630"
+    };
+    firebase.initializeApp(config);
+    // Get a reference to the database service
+    database = firebase.database();
+
+    //var userId = firebase.auth().currentUser.uid;
+}
--- public/js/index.js
+++ public/js/index.js
@@ -1,20 +1,34 @@
 $(document).ready(function () {
+    //Connect to Firebase Database
+    connectDB();
+
     window.onscroll = function () {
-        myFunction()
+        highlightNav()
     };
 
-    function myFunction() {
-        console.log("My function")
-        if (document.body.scrollTop > 200
-            //|| document.documentElement.scrollTop > 200
-        ) {
-            console.log("inside class add");
-            $(".navbar").addClass('navbar-fixed-top highlight');
-            //$(".navbar").addClass('highlight');
-            //            
-        } else {
-            $(".navbar").removeClass('navbar-fixed-top highlight');
-            //$(".navbar").removeClass('highlight');
+    // Fetch Images from Database and add to Main page
+    fetchImages().then((imageList) => {
+        for (var i = 1; i < imageList.length; i++) {
+            console.log(JSON.stringify(imageList[i]));
+            console.log(i + "--" + imageList[i].url);
         }
-    }
+    });
+
+    var $grid = $('.grid').imagesLoaded(function () {
+        $grid.masonry({
+            itemSelector: '.grid-item',
+            percentPosition: true,
+            columnWidth: '.grid-sizer'
+        });
+    });
+
 });
+
+function highlightNav() {
+    if (document.body.scrollTop > 200) {
+        console.log("inside class add");
+        $(".navbar").addClass('navbar-fixed-top highlight');
+    } else {
+        $(".navbar").removeClass('navbar-fixed-top highlight');
+    }
+}
