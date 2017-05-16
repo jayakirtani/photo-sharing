4b5c2c09247cd8b8d6daa73b4d7f74a4e221d7af
GHOST\imran <imran.vatic@gmail.com>
2017-05-15 17:08:55 -0700

    Added the code for displaying pop-up on users page with comments and likes

--- public/js/user.js
+++ public/js/user.js
@@ -3,6 +3,7 @@ var storageRef = firebase.storage().ref();
 $body = $("body");
 $(document).ready(function (){
 
+var batchSize= 8;
 window.onscroll = function () {
         highlightNav()
     };
@@ -23,29 +24,59 @@ $('#hello').replaceWith("<h2>Hello "+user.displayName +"</h2>");
         percentPosition: true,
         columnWidth: '.grid-sizer',
     });
-    retrieveImages().then((imageList) => {
-        for (var key in imageList) {
-                var userimagedata = imageList[key].url;
+    // retrieveImages().then((imageList) => {
+    //     for (var key in imageList) {
+    //             var userimagedata = imageList[key].url;
                 
-                var likes = imageList[key].likes;
-                var comments = imageList[key].totalcomments;
-                var $griditem = $(`<div class="grid-item" id="${key}">
-                <img src="${userimagedata}" />
-                <div id="userimageinfo">
-                <span>${comments} Comments</span>
-                <span>${likes} Likes</span>
-                </div>
-            </div>`)
-            // Add images to the image grid
-            $('.grid').prepend($griditem).isotope('prepended', $griditem);
-        }
+    //             var likes = imageList[key].likes;
+    //             var comments = imageList[key].totalcomments;
+    //             var $griditem = $(`<div class="grid-item" id="${key}">
+    //             <img src="${userimagedata}" />
+    //             <div id="userimageinfo">
+    //             <span>${comments} Comments</span>
+    //             <span>${likes} Likes</span>
+    //             </div>
+    //         </div>`)
+    //         // Add images to the image grid
+    //         $('.grid').prepend($griditem).isotope('prepended', $griditem);
+    //     }
 
-        // Rearrange the layout once all images have loaded
-        $('.grid').imagesLoaded(function () {
-            console.log("all images loaded")
-            $('.grid').isotope('layout');
-        });
+    //     // Rearrange the layout once all images have loaded
+    //     $('.grid').imagesLoaded(function () {
+    //         console.log("all images loaded")
+    //         $('.grid').isotope('layout');
+    //     });
+    // });
+
+    /////////////////////////////////////////////////////
+ fetchUserFirstImageBatch(batchSize).then((imageList) => {
+        showUserImages(false, imageList);
     });
+    $(document).endlessScroll({
+        inflowPixels: 0,
+        fireOnce: true,
+        fireDelay: true,
+        //loader: "Loading...",
+        ceaseFireOnEmpty: false,
+        callback: function (i, p, d) {
+            console.log('Inside callback');
+            if (d === 'next') {
+                console.log("Window down Scrolled");
+                fetchUserNextImageBatch(batchSize).then((imageList) => {
+                    showUserImages(true, imageList);
+                });
+            }
+        }
+        // ,
+        // ceaseFire : function(i,p,d){
+        //     if (d ==='prev'){
+        //         return true;
+        //     }
+        // }
+    });
+
+
+    /////////////////////////////////////////////////////
 
   } else {
     // No user is signed in.
@@ -90,31 +121,37 @@ function handleFileSelect(evt) {
             totalcomments:ititialComments,
             likes:initialLikes,
             timestamp:-Date.now()
-        });
-
-        firebase.database().ref('/images/').push({
+        }).then((snap)=>{
+            const key = snap.key;
+            firebase.database().ref('images').child(key).set({
             url: url,
             totalcomments:ititialComments,
             likes:initialLikes,
             timestamp:-Date.now()
+            
         });
+    window.location ="user.html";  
+    });
 
         $body.removeClass("loading");
-                var $griditem = $(`<div class="grid-item" id="${uid}">
-                <img src="${url}" />
-                <div id="userimageinfo">
-                <span>${ititialComments} Comments</span>
-                <span>${initialLikes} Likes</span>
-                </div>
-            </div>`)
-            console.log($griditem);
-            // Add images to the image grid
-            $('.grid').prepend($griditem).isotope('prepended', $griditem);
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+            //     var $griditem = $(`<div class="grid-item" id="${uid}">
+            //     <img src="${url}" />
+            //     <div id="userimageinfo">
+            //     <span>${ititialComments} Comments</span>
+            //     <span>${initialLikes} Likes</span>
+            //     </div>
+            // </div>`)
+            // console.log($griditem);
+            // // Add images to the image grid
+            // $('.grid').prepend($griditem).isotope('prepended', $griditem);
 
-            $('.grid').imagesLoaded(function () {
-                        console.log("all images loaded")
-                        $('.grid').isotope('layout');
-              });
+            // $('.grid').imagesLoaded(function () {
+            //             console.log("all images loaded")
+            //             $('.grid').isotope('layout');
+            //   });
+
+      /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////         
               document.getElementById('image_file').disabled = false;
         console.log(uid);
       }).catch(function(error) {
@@ -137,3 +174,125 @@ function handleFileSelect(evt) {
     return uuid;
 };
 	
+
+function showUserImages(ignorefirst, imageList) {
+
+    for (var i = 0; i < imageList.length; i++) {
+        if (!ignorefirst) {
+            var imagedata = imageList[i];
+            //console.log("Appending Image " + JSON.stringify(imagedata));
+            var key = Object.keys(imagedata);
+            var data = imagedata[key];
+            var commentKeys = [];
+            data.comments = data.comments || [];
+            var commentsListStr = '';
+            try {
+                commentKeys = Object.keys(data.comments);
+                commentKeys.forEach(function(key) {
+                    var comment = data.comments[key];
+                    commentsListStr += $(`<li>
+                        <span class="username">${comment.username}</span> says -
+                        <span class="content">${comment.text}</span></li>`)[0].outerHTML;
+                }, this);
+            } catch (e) {
+
+            }
+
+            var $griditem = $(`<div class="grid-item" data-featherlight="#inner-${key}" id="${key}">
+                <div id="inner-${key}" class="inner-grid-content">
+                <img class="img-responsive" src="${data.url}" />
+                <div id="imageinfo" class="likes">
+                <span><i class="comments-count fa fa-comments">${commentKeys.length}</i></span>
+                <span class="empty"></span>
+                <span><i class="likes-count fa fa-heart">${data.likes}  </i> </span>
+                </div>
+                <section class="comments-section">
+                    <span class="likes-count">${data.likes} </span><span onclick="addUserLike('${key}', this)" class="fa fa-heart"></span>
+                    <span class="comments-count">${commentKeys.length} </span><span class="fa fa-comments"></span>
+                    <div class="form-group">
+                        <label for="comment">Comment:</label>
+                        <textarea class="form-control" rows="5" id="comment${key}" data-unique="i${key}"></textarea>
+                    </div>
+
+                    <button class="btn btn-primary" onclick="addUserComment('${key}', this)">Submit</button>
+                    <ul class="comments-list">${commentsListStr}</ul>
+                </section>
+                </div></div>`);
+            //console.log($griditem);
+            // Add images to the image grid
+            $('.grid').append($griditem).isotope('appended', $griditem).isotope('layout');
+        } else {
+            // Ignored first Image as it was already displayed in last batch
+            ignorefirst = false;
+        }
+    }
+
+
+    // Rearrange the layout once all images have loaded
+    $('.grid').imagesLoaded().progress(function () {
+        //console.log("all images loaded")
+        $('.grid').isotope('layout');
+            $('.grid-item').featherlightGallery({
+                previousIcon: '«',
+                nextIcon: '»',
+                galleryFadeIn: 300,
+                openSpeed: 300
+            });    });
+}
+
+function addUserComment(imageId, elem) {
+	var commentRef = firebase.database().ref('/images/' + imageId).child("comments");
+	var newPostRef = commentRef.push();
+    console.log("Comment adeed-", $("textarea[data-unique='i"+ imageId +"']")[1].value);
+    var newComment = {
+        text : $("textarea[data-unique='i"+ imageId +"']")[1].value,
+        username : firebase.auth().currentUser.displayName,
+        ts : Date.now()
+    };
+    newPostRef.set(newComment);
+    //Append to UI.
+   
+
+updateCommentInUser(imageId,elem);
+}
+//update the user database also
+function updateCommentInUser(imageId,elem){
+    var uid = firebase.auth().currentUser.uid;
+var commentRef = firebase.database().ref('/users/' + uid+'/images/' + imageId).child("comments");
+	var newPostRef = commentRef.push();
+    console.log("Comment adeed-", $("textarea[data-unique='i"+ imageId +"']")[1].value);
+    var newComment = {
+        text : $("textarea[data-unique='i"+ imageId +"']")[1].value,
+        username : firebase.auth().currentUser.displayName,
+        ts : Date.now()
+    };
+    newPostRef.set(newComment);
+    //Append to UI.
+    var commentStr = $(`<li>
+            <span class="username">${newComment.username}</span> says -
+            <span class="content">${newComment.text}</span></li>`)[0].outerHTML;
+    $(".comments-list").prepend($(commentStr));
+    //Update count on UI - popup.
+    var commentCount = $(elem).siblings('span.comments-count');
+    var newCount = parseInt(commentCount.text().trim()) + 1
+     commentCount.text(newCount);
+     //Update on homepage too.
+     $("#" + imageId).find(".comments-count").eq(0).text(newCount);
+}
+
+function addUserLike(imageId, elem) {
+    var likeCount = parseInt($(elem).siblings('span.likes-count').text().trim()) + 1;
+    firebase.database().ref('/images/' + imageId).update({likes : likeCount});
+    $(elem).siblings('span.likes-count').text(likeCount);
+    //Update on homepage too.
+     $("#" + imageId).find(".likes-count").eq(0).text(likeCount);
+
+     updateLikesInUser(imageId,elem);
+}
+
+function updateLikesInUser(imageId,elem)
+{
+  var uid = firebase.auth().currentUser.uid;
+  var likeCount = parseInt($(elem).siblings('span.likes-count').text().trim());
+  var commentRef = firebase.database().ref('/users/' + uid+'/images/' + imageId).update({likes : likeCount}); 
+}
--- public/js/userdb.js
+++ public/js/userdb.js
@@ -6,3 +6,44 @@ function retrieveImages() {
         return Promise.resolve(imageList.val());
     });
 }
+
+
+function fetchUserFirstImageBatch(batchsize) {
+    var uid = firebase.auth().currentUser.uid;
+    return new Promise(function (fulfill, reject) {
+        firebase.database().ref('users/' + uid+'/images/').orderByChild('timestamp').limitToFirst(batchsize).on('value', function (list) {
+            console.log("First Batch");
+            //console.log(list.val()); // JSON with all image data
+            //console.log(list.key); //images
+            var orderedData = [];
+            // Loop through each JSON Object inside list and push the data in
+            // an array to maintain order
+            list.forEach(function (child) {
+                var dataObject = {};
+                dataObject[child.key] = child.val();
+                orderedData.push(dataObject);
+                lastImageTimestamp = child.val().timestamp;
+            });
+            console.log("lastImageTimestamp" + lastImageTimestamp);
+            fulfill(orderedData);
+        });
+    });
+}
+
+function fetchUserNextImageBatch(batchsize) {
+    var uid = firebase.auth().currentUser.uid;
+    return new Promise(function (fulfill, reject) {
+        firebase.database().ref('users/' + uid+'/images/').orderByChild('timestamp').startAt(lastImageTimestamp).limitToFirst(batchsize).on('value', function (imageList) {
+            console.log("Next Batch");
+            var orderedData = [];
+            imageList.forEach(function (child) {
+                var dataObject = {};
+                dataObject[child.key] = child.val();
+                orderedData.push(dataObject);
+                lastImageTimestamp = child.val().timestamp;
+            });
+            fulfill(orderedData);
+            //fulfill(imageList.val());
+        });
+    });
+}
